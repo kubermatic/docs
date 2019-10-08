41889c95c289f16bc0b7d96c30f52ba6d6eadefc
Christoph Mewes <christoph@loodse.com>
2019-10-08 14:58:56 +0200

    add remark-lint Docker image and hack script

---
+++ hack/ci/lint-markdown.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+cd $(dirname "$0")/../..
+
+echo "Running remark-lint on the content directory..."
+remark --quiet --frail content
+echo "No problems found, good job :-)"
