d9016e9d5c72e7e157dda9c641739b13f9eaf625
Irina Lindt <irina@loodse.com>
2020-07-27 14:42:16 +0200

    added actual_retry and write_junit from lib.sh

--- hack/ci/e2e-tests.sh
+++ hack/ci/e2e-tests.sh
@@ -62,7 +62,54 @@ retry() {
   return $rc
 }
 
+actual_retry() {
+  retries=$1 ; shift
+
+  count=0
+  delay=1
+  until "$@"; do
+    rc=$?
+    count=$(( count + 1 ))
+    if [ $count -lt "$retries" ]; then
+      echo "Retry $count/$retries exited $rc, retrying in $delay seconds..." >/dev/stderr
+      sleep $delay
+    else
+      echo "Retry $count/$retries exited $rc, no more retries left." >/dev/stderr
+      return $rc
+    fi
+    delay=$(( delay * 2 ))
+  done
+  return 0
+}
+
 TEST_NAME="Get Vault token"
+
+write_junit() {
+  # Doesn't make any sense if we don't know a testname
+  if [ -z "${TEST_NAME:-}" ]; then return; fi
+  # Only run in CI
+  if [ -z "$ARTIFACTS" ]; then return; fi
+
+  rc=$1
+  duration=${2:-0}
+  errors=0
+  failure=""
+  if [ "$rc" -ne 0 ]; then
+    errors=1
+    failure='<failure type="Failure">Step failed</failure>'
+  fi
+  TEST_NAME="[Kubermatic] ${TEST_NAME#\[Kubermatic\] }"
+  cat <<EOF > ${ARTIFACTS}/junit.$(echo $TEST_NAME|sed 's/ /_/g').xml
+<?xml version="1.0" ?>
+<testsuites>
+    <testsuite errors="$errors" failures="$errors" name="$TEST_NAME" tests="1">
+        <testcase classname="$TEST_NAME" name="$TEST_NAME" time="$duration">
+          $failure
+        </testcase>
+    </testsuite>
+</testsuites>
+EOF
+}
 echodate "Getting secrets from Vault"
 export VAULT_ADDR=https://vault.loodse.com/
 export VAULT_TOKEN=$(vault write \
