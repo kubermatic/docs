536353774fbaa82d2664d62b65fe913479b5389e
serg <contact@sergfurtak.com>
2022-07-11 16:51:14 +0300

    remove comment with jquery code
    Signed-off-by: serg <contact@sergfurtak.com>

--- assets/js/app.js
+++ assets/js/app.js
@@ -222,37 +222,3 @@ window.addEventListener('DOMContentLoaded', function() {
   imgLightbox('img-lightbox-link', {rate: 10});
   initExpandShortcode();
 });
-
-// Add link button for every
-/*
-function() {
-  var text, clip = new ClipboardJS('.anchor');
-  $("h1~h2,h1~h3,h1~h4,h1~h5,h1~h6").append(function(index, html){
-    var element = $(this);
-    var url = encodeURI(document.location.origin + document.location.pathname);
-    var link = url + "#"+element[0].id;
-    return " <span class='anchor' data-clipboard-text='"+link+"'>" +
-      "<i class='fas fa-link fa-lg'></i>" +
-      "</span>"
-    ;
-  });
-
-  $(".anchor").on('mouseleave', function(e) {
-    $(this).attr('aria-label', null)
-           .removeClass('tooltipped tooltipped-s tooltipped-w');
-  });
-
-  clip.on('success', function(e) {
-    e.clearSelection();
-    $(e.trigger).attr('aria-label', 'Link copied to clipboard!')
-                .addClass('tooltipped tooltipped-s');
-  });
-
-  $('code.language-mermaid').each(function(index, element) {
-    var content = $(element).html()
-                            .replace(/&amp;/g, '&');
-    $(element).parent()
-              .replaceWith('<div class="mermaid" align="center">' + content + '</div>');
-  });
-}
-*/
